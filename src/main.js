1b4d702ec2f0f0fc8b7d4df84ef81133e6e7f140
NesterovaAnna <nesterova.anna.alex@yandex.ru>
2024-12-10 22:24:12 +0600

    3, 4 level

--- src/main.js
+++ src/main.js
@@ -426,7 +426,297 @@ scene("level_02", async () => {
 
 })
 
+scene("level_03", async () => {
+    let bg = add([
+        sprite("shop-full", {width: width(), height: height()})
+    ])
 
+    function getCard(name, age, photoName, description) {
+        let card = make([
+            sprite("card-back", {width: 400, height: 400}),
+            pos(center()),
+            area(),
+            anchor("center"),
+            timer(),
+            "card",
+            {name: photoName}
+        ])
+
+        let photo = card.add([
+            sprite(photoName, {width: 100, height: 100}),
+            anchor("center"),
+            pos(0, -70),
+        ])
+
+        let info = card.add([
+            text(`${name} - ${age} лет`, {width: 300, size: 26, align: "center"}),
+            color(0, 0, 0),
+            anchor("center"),
+            pos(0, 0),
+        ])
+
+        let resume = card.add([
+            text(description, {width: 250, size: 20, align: "center"}),
+            anchor("center"),
+            color(0, 0, 0),
+            pos(0, 50),
+        ])
+
+        return card
+    }
+    add(getCard("Вася", 25, "vasya", "Работал на стройке, сомнительная репутация, но готов работать задёшево"))
+    add(getCard("Петя", 18, "petya", "Студент, готов работать сверхурочно, крепкого телосложения"))
+    add(getCard("Маша", 30, "masha", "Работала мерчендайзером в супермаркете 3 года"))
+    add(getCard("Даша", 87, "dasha", "Была библиотекарем до закрытия места работы, плохо слышит"))
+    add(getCard("Кирилл", 22, "kirill", "Рок-музыкант, который ищет постоянное место работы"))
+
+    let talking = false;
+    let gotPeople = 0;
+
+    let caller = add([
+        "callerOfEvents"
+    ])
+    let success = {
+        "vasya": "Его сомнительная репутация... Как бы не пришлось за ним еще следить, будь готов перепроверять счета и поставки!",
+        "petya": "Сразу видно, если что поможет и коробки с книгами перетащить, и за магазином приглядеть",
+        "masha": "Это правильно, человек с опытом в сфере работы магазинов не помешает",
+        "dasha": "Уважение к старости - это похвально, но проблемы со слухом могут стать нашими ",
+        "kirill": "Сомнительно, но ок, я бы предложила поработать этому молодому человеку в магазине музыки",
+    }
+
+    let declines = {
+        "vasya": "Хорошо, нашему магазину не нужна сомнительная репутация",
+        "petya": "Не совсем понимаю причину отказа, ведь он бы мог быть хорошим работником",
+        "masha": "Грубейшая ошибка! Она могла бы очень сильно помочь и с документацией, да и по магазину поддерживать порядок",
+        "dasha": "При всем уважении, она заслуживает отдыха",
+        "kirill": "Верно, очень важно следить за внешним видом, книжный магазин предполагает более сдержанный стиль",
+    }
+
+    caller.on("success", async e => {
+        talking = true
+        await sayText(success[e], "aunt")
+        gotPeople += 1
+        caller.trigger("complete")
+        talking = false
+    })
+
+    caller.on("decline", async e => {
+        talking = true
+        await sayText(declines[e], "aunt")
+        caller.trigger("complete")
+        talking = false
+    })
+
+    caller.on("complete", async e => {
+        if (get("card").length === 0) {
+            if (gotPeople <= 1) await sayText("Что ж, я бы на твоем месте набрала больше персонала, потому что работать самому здорово - ближе к людям, но все же не совсем эффективно", "aunt")
+            else if (gotPeople === 2) await sayText("Хорошо, самое то количество рабочих для старта магазина!", "aunt")
+            else await sayText("Ты очень щедр, но сомневаюсь, что столько работников потребуется, но да ладно, время покажет", "aunt")
+            await sayText("Хах, ты выбираешь людей? Самые надежные рабочие - твои друзья! С ними и работа в кайф, да и после работы можно затусить", "friend")
+            await sayText("Ты все делаешь правильно, дорогой, вот увидишь!", "aunt")
+            goWithFade("level_04")
+        }
+    })
+
+    onKeyPress("right", () => {
+        if (talking) return;
+
+        let cards = get("card")
+        if (!cards) return;
+
+        let card = cards[cards.length - 1]
+        card.tween(card.pos, vec2(width() + 200, card.pos.y), 0.25, v => card.pos = v)
+        card.unuse("card")
+        caller.trigger("success", card.name)
+    })
+
+    onKeyPress("left", () => {
+        if (talking) return;
+
+        let cards = get("card")
+        if (!cards) return;
+
+        let card = cards[cards.length - 1]
+        if (!card) return;
+
+        card.tween(card.pos, vec2(-200, card.pos.y), 0.25, v => card.pos = v)
+        card.unuse("card")
+        caller.trigger("decline", card.name)
+    })
+
+    let decliner = add([
+        sprite("up"),
+        pos(center().x - 100, height() - 40),
+        rotate(-90),
+        scale(3),
+        area(),
+        anchor("center")
+    ])
+    add([
+        text("Отклонить", {size: 20}),
+        pos(center().x - 100, height() - 70),
+        anchor("center")
+    ])
+
+    decliner.onClick(() => {
+        if (talking) return;
+
+        let cards = get("card")
+        if (!cards) return;
+
+        let card = cards[cards.length - 1]
+        if (!card) return;
+        card.tween(card.pos, vec2(-200, card.pos.y), 0.25, v => card.pos = v)
+        card.unuse("card")
+        caller.trigger("decline", card.name)
+    })
+
+    let accepter = add([
+        sprite("up"),
+        pos(center().x + 100, height() - 40),
+        rotate(90),
+        scale(3),
+        area(),
+        anchor("center")
+    ])
+    add([
+        text("Принять", {size: 20}),
+        pos(center().x + 100, height() - 70),
+        anchor("center")
+    ])
+    accepter.onClick(() => {
+        if (talking) return;
+
+        let cards = get("card")
+        if (!cards) return;
+
+        let card = cards[cards.length - 1]
+        card.tween(card.pos, vec2(width() + 200, card.pos.y), 0.25, v => card.pos = v)
+        card.unuse("card")
+        caller.trigger("success", card.name)
+    })
+
+
+    talking = true
+    await sayText("Теперь давай подберем персонал", "aunt")
+    await sayText("Моя подруга подкинула резюме знакомых", "aunt")
+    await sayText("Не бери слишком много - много денег потеряешь на зарплату...", "aunt")
+    await sayText("А еще смотри на прошлые заслуги человека, они очень важны!", "aunt")
+    talking = false
+})
+
+scene("level_04", async () => {
+    let goodCount = 0;
+    let badCount = 0;
+
+    function createCard(spriteName, textToWrite, position, good) {
+        let active = true;
+        let op = 1
+
+        if (good) goodCount++;
+        else badCount++;
+
+        let card = make([
+            rect(300, 180),
+            anchor("center"),
+            pos(position),
+            area(),
+            opacity(1),
+            color(209, 177, 138),
+            {name: spriteName},
+            "card"
+        ])
+
+        let image = card.add([
+            sprite(spriteName, {width: 200}),
+            anchor("center"),
+            pos(0, -10),
+            opacity(1)
+        ])
+
+        let txt = card.add([
+            text(textToWrite, {width: 250, size: 20, align: "center"}),
+            color(0, 0, 0),
+            pos(0, 80),
+            anchor("center"),
+            opacity(1),
+        ])
+
+        onMouseDown(() => {
+            if (talking) return;
+            if (!card.isClicked()) return;
+            card.trigger("change")
+        })
+
+        card.on("change", () => {
+            if (active) {
+                if (good) goodCount--;
+                else badCount--;
+                op = 0.2
+                active = false;
+            } else {
+                if (good) goodCount++;
+                else badCount++;
+                op = 1
+                active = true;
+            }
+
+            card.opacity = op
+            image.opacity = op
+            txt.opacity = op
+        })
+
+        return card
+    }
+
+    add([
+        sprite("gazeta", {width: width(), height: height()}),
+        pos(center()),
+        anchor("center")
+    ])
+
+    add(createCard("amogus", "Популярная игра во всех чартах!", vec2(center().x - 400, 100), false))
+    add(createCard("aviasales", "Дешевые билеты - АвиаСейлс!", vec2(center().x - 400, 600), false))
+    add(createCard("gorod", "Популярность книг в городе возросла\n © BookPoint", vec2(center().x - 200, 300), true))
+    add(createCard("grafik", "Книги доступны как никогда прежде в новом магазине!", vec2(center().x + 400, 100), true))
+    add(createCard("hamburger", "Бургеры от Равшана -50%", vec2(center().x + 400, 600), false))
+    add(createCard("krug", "Iq читающих книги выше - доказано!", vec2(center().x + 200, 300), true))
+    add(createCard("ruki", "Договор с японцами на поставку суши", vec2(center().x, 600), false))
+
+    let submit = add([
+        anchor("center"),
+        text("Отправить", {size: 40}),
+        pos(width() * 0.75, height() - 50),
+        color(0, 0, 0),
+        area()
+    ])
+    onMouseDown(async () => {
+        if (!submit.isClicked()) return;
+        talking = true
+        if (badCount > 0) {
+            await sayText("Хм... Кажется, ты добавил в нашу статью лишние фрагменты", "aunt")
+            await sayText("Подумай еще раз хорошенько!", "aunt")
+        } else if (goodCount !== 3) {
+            await sayText("Хм... Кажется, ты не добавил в нашу статью фрагменты, которые нам подходят", "aunt")
+            await sayText("Подумай еще раз хорошенько!", "aunt")
+        } else {
+            await sayText("Здорово! Самое время отправить нашу статью в газету!", "aunt")
+            await sayText("Газету? А может еще бумажки раздавать? Ну нееет, мое кафе сами люди разрекламируют!", "friend")
+            await sayText("Посмотрим, как скоро он пожалеет о своем решении", "aunt")
+            goWithFade("level_05")
+        }
+
+        talking = false;
+    })
+
+    talking = true
+    await sayText("Хорошо, начало положено", "aunt")
+    await sayText("Теперь стоит задуматься о том, как люди узнают о нас", "aunt")
+    await sayText("Что же будем делать?", "me")
+    await sayText("У меня есть знакомый, который работает в газете. Напишем ему, только нужно собрать статью", "aunt")
+    await sayText("Я видел вырезки из газет, нужно только собрать нужные", "me")
+    talking = false
+})
 
 scene("level_05", async () => {
     let map = add([
